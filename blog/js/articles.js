f5631905e667560cae7c64eab67eea1d46897fad
Mahmoud Atwa <m.atwa@icloud.com>
2021-05-02 03:49:01 +0200

    Added OOP post

--- blog/js/articles.js
+++
@@ -1,4 +0,0 @@
-const articles=[{title:'Complexity Analysis',description:`A general approach for analyzing algorithms, 
-                    with a discussion on Big O notation and the mathematics behind it.
-                    Including some examples on both practical and theoritcal aspects of the concept 
-                    & other related concepts that are minimally discussed.`,location:'./articles/complexity/',pdfLoc:'./articles/complexity/assets/article.pdf',thumbnail:'./articles/complexity/assets/thumb.png'}]
